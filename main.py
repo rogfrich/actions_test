78934a1df840a896ab0042e451db37511976d784
Rogfrich <richard.a.cairns@gmail.com>
2020-10-23 16:24:05 +0100

    simple

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime, os
-with open(f'{datetime.datetime.now()}.txt', 'w') as f:
-    f.write(f'written from {os.path.abspath(__file__)}')
+with open("readme.md") as f:
+    pass
 
-print("Hello from the script")
